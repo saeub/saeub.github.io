77f7b96ac14c32f6ac704cd6df55a2a00a21b4bf
Andreas Säuberli <andreas.saeuberli@gmail.com>
2024-04-12 17:35:22 +0200

    Use JEKYLL_ENV=development in Docker Compose setup

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 CONFIG_FILE=_config.yml
-CONFIG_FILE_DEV=_config-dev.yml
 
-/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --config $CONFIG_FILE,$CONFIG_FILE_DEV --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
 while true; do
 
-  inotifywait -q -e modify,move,create,delete $CONFIG_FILE,$CONFIG_FILE_DEV
+  inotifywait -q -e modify,move,create,delete $CONFIG_FILE
 
   if [ $? -eq 0 ]; then
 
-    echo "Change detected to $CONFIG_FILE or $CONFIG_FILE_DEV, restarting Jekyll"
+    echo "Change detected to $CONFIG_FILE, restarting Jekyll"
 
     jekyll_pid=$(pgrep -f jekyll)
     kill -KILL $jekyll_pid
 
-    /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --config $CONFIG_FILE,$CONFIG_FILE_DEV --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+    /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
   fi
 
